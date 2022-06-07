e7fbb217172922a8774ed29ba7f24d165f3b6976
Dustin Li <dustin.li1988@gmail.com>
2022-06-07 20:03:06 +0000

    Redesign collapsible layout, add colour to seperate collapsible headers

--- static/js/script.js
+++ static/js/script.js
@@ -12,6 +12,13 @@ $(document).ready(function(){
         }
     });
 
+
+    var elem = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elem, {
+    accordion: false
+    });
+            
+
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
