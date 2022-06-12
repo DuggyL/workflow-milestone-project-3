c7b00c56bba51a3fc74c016523e9838613aed556
Dustin Li <dustin.li1988@gmail.com>
2022-06-12 12:19:23 +0000

    Add more dataset to form on add task

--- app.py
+++ app.py
@@ -104,11 +104,17 @@ def add_task():
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
         task = {
-            "category_name": request.form.get("category_name"),
-            "task_name": request.form.get("task_name"),
-            "task_description": request.form.get("task_description"),
+            "job_title": request.form.get("job_title"),
+            "company_name": request.form.get("company_name"),
+            "job_description": request.form.get("job_description"),
+            "quantity": request.form.get("quantity"),
+            "materials": request.form.get("materials"),
+            "printing": request.form.get("printing"),
+            "finishing": request.form.get("finishing"),
+            "delivery": request.form.get("delivery"),
+            "delivery_date": request.form.get("delivery_date"),
+            "delivery_address": request.form.get("delivery_address"),
             "is_urgent": is_urgent,
-            "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
         mongo.db.tasks.insert_one(task)
